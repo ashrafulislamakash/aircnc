b93644580f08b866983537b48ac7384847b1e082
ashrafulislamakash <ashrafulislam.eng@gmail.com>
2023-08-06 21:33:20 +0600

    add form addded

--- src/Pages/Dashboard/AddRoom.jsx
+++ src/Pages/Dashboard/AddRoom.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from "react";
+import AddRoomForm from "../../components/Forms/AddRoomForm";
 
 const AddRoom = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [loading, setLoading] = useState(false);
+  const [uploadButtonText, setUploadButtonText] = useState("Upload Image");
+
+  // handleSubmit
+  const handleSubmit = (event) => {
+    setLoading(true);
+    event.preventDefault();
+    const location = event.target.location.value;
+    const title = event.target.title.value;
+    const from = dates.startDate;
+    const to = dates.endDate;
+    const price = event.target.price.value;
+    const total_guest = event.target.total_guest.value;
+    const bedrooms = event.target.bedrooms.value;
+    const bathrooms = event.target.bathrooms.value;
+    const description = event.target.description.value;
+    const category = event.target.category.value;
+
+    const image = event.target.image.files[0];
+  };
+
+  const handleImageChange = (image) => {
+    setUploadButtonText(image.name);
+  };
+  return (
+    <AddRoomForm
+      handleSubmit={handleSubmit}
+      loading={loading}
+      handleImageChange={handleImageChange}
+      uploadButtonText={uploadButtonText}
+    />
+  );
 };
 
 export default AddRoom;
